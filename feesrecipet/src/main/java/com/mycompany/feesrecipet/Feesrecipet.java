03b4762f00ad010b0444494a435a74c8b1b57ec9
Abhishek-8755 <83465711+Abhishek-8755@users.noreply.github.com>
2024-02-11 12:33:51 +0530

    Add files via upload

---
+++ feesrecipet/src/main/java/com/mycompany/feesrecipet/Feesrecipet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feesrecipet;
+
+/**
+ *
+ * @author Abhi
+ */
+public class Feesrecipet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
